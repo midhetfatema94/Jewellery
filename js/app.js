29d5309e09471020f879295c491dd458fbfb28a0
midhetfatema94 <midhetfatema94@gmail.com>
2016-09-16 23:22:22 +0530

    initial commit

---
+++ js/app.js
@@ -0,0 +1,80 @@
+// JavaScript Document
+var firstapp = angular.module('firstapp', [
+  'ui.router',
+  'phonecatControllers',
+  'templateservicemod',
+  'navigationservice',
+  'pascalprecht.translate',
+  'angulartics'
+]);
+
+firstapp.config(function($stateProvider, $urlRouterProvider, $httpProvider, $locationProvider) {
+  // for http request with session
+  $httpProvider.defaults.withCredentials = true;
+  $stateProvider
+    .state('home', {
+      url: "/",
+      templateUrl: "views/template.html",
+      controller: 'HomeCtrl'
+    })
+    .state('newTab', {
+      url: "/new",
+      templateUrl: "views/template.html",
+      controller: 'NewTabCtrl'
+    });
+  $urlRouterProvider.otherwise("/");
+  $locationProvider.html5Mode(isproduction);
+});
+
+
+firstapp.directive('img', function($compile, $parse) {
+  return {
+    restrict: 'E',
+    replace: false,
+    link: function($scope, element, attrs) {
+      var $element = $(element);
+      if (!attrs.noloading) {
+        $element.after("<img src='img/loading.gif' class='loading' />");
+        var $loading = $element.next(".loading");
+        $element.load(function() {
+          $loading.remove();
+          $(this).addClass("doneLoading");
+        });
+      } else {
+        $($element).addClass("doneLoading");
+      }
+    }
+  };
+});
+
+firstapp.directive('fancyboxBox', function($document) {
+    return {
+        restrict: 'EA',
+        replace: false,
+        link: function(scope, element, attr) {
+            var $element = $(element);
+            var target;
+            if (attr.rel) {
+               target = $("[rel='" + attr.rel + "']");
+            } else {
+                target = element;
+            }
+
+            target.fancybox({
+                openEffect: 'fade',
+                closeEffect: 'fade',
+                closeBtn: true,
+                helpers: {
+                    media: {}
+                }
+            });
+        }
+    };
+});
+
+
+firstapp.config(function ($translateProvider) {
+  $translateProvider.translations('en', LanguageEnglish);
+  $translateProvider.translations('hi', LanguageHindi);
+  $translateProvider.preferredLanguage('en');
+});
---
+++ js/navigation.js
@@ -0,0 +1,70 @@
+var adminURL = "";
+if(isproduction)
+{
+  adminURL =  "http://www.wohlig.co.in/demo/index.php";
+}
+else {
+  adminURL = "http://localhost/demo/index.php";
+}
+
+var navigationservice = angular.module('navigationservice', [])
+
+.factory('NavigationService', function() {
+  var navigation = [{
+    name: "Jewellery",
+    classis: "active",
+    anchor: "home",
+    subnav: [{
+      name: "Subnav1",
+      classis: "active",
+      anchor: "home"
+    }]
+  },
+  {
+    name: "About",
+    classis: "active",
+    anchor: "newTab"
+  },
+  {
+    name: "Shop",
+    classis: "active",
+    anchor: "newTab"
+  },
+  {
+    name: "Custom",
+    classis: "active",
+    anchor: "newTab"
+  },
+  {
+    name: "Retailers",
+    classis: "active",
+    anchor: "newTab"
+  },
+  {
+    name: "Journal",
+    classis: "active",
+    anchor: "newTab"
+  }
+  {
+    name: "Contact",
+    classis: "active",
+    anchor: "newTab"
+  }];
+
+  return {
+    getnav: function() {
+      return navigation;
+    },
+    makeactive: function(menuname) {
+      for (var i = 0; i < navigation.length; i++) {
+        if (navigation[i].name == menuname) {
+          navigation[i].classis = "active";
+        } else {
+          navigation[i].classis = "";
+        }
+      }
+      return menuname;
+    },
+
+  };
+});
